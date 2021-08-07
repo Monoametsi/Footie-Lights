2bd0705cfa304ca0080e9e9a9899c5cf743f8e4d
Junior <216026633@student.uj.ac.za>
2021-08-07 19:42:07 +0200

    Updated files

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 const path = require('path');
+const fetch = require('node-fetch');
 const dirname = __dirname.slice(0, __dirname.search(/Server/i) - 1);
 
 app.disable('etag');
@@ -14,6 +15,27 @@ app.use(express.static(path.join(dirname)));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+	
+	let footballHighlightData = `https://www.scorebat.com/video-api/v3/`;
+	
+	fetch(footballHighlightData, { 
+		method: 'GET'
+	} ).then( async (res) => {
+		let response = await res.json();
+		let results = response.response;
+		
+		//if(response.ok){
+			for(i = 0; i < results.length; i++){
+				console.log(results[i].title);
+			}
+		//}
+		
+	}).catch((err) => {
+		
+		console.log(err);
+		
+	});
+	
 	res.status(200).render('index');
 });
 
